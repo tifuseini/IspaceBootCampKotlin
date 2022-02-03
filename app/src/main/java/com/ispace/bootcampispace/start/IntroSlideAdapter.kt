9dff25cb464ca8bcb2fcdc5be263bd67ad06519e
tifuseini <tifuseini@st.ug.edu.gh>
2022-02-03 22:44:44 +0000

    Added IntroActivity

--- app/src/main/java/com/ispace/bootcampispace/start/IntroSlideAdapter.kt
+++ app/src/main/java/com/ispace/bootcampispace/start/IntroSlideAdapter.kt
@@ -1,23 +1,26 @@
 package com.ispace.bootcampispace.start
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.ispace.bootcampispace.R
 
-class IntroSlideAdapter(
-    private val introSlide: List<IntroSlide>
-) :RecyclerView.Adapter<IntroSlideAdapter.IntroSlideViewHolder>() {
+class IntroSliderAdapter(private val introSlide: List<IntroSlide>) :RecyclerView.Adapter<IntroSliderAdapter.IntroSlideViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): IntroSlideViewHolder {
-        TODO("Not yet implemented")
+        return IntroSlideViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.slide_item_container,parent,false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: IntroSlideViewHolder, position: Int) {
-
+        holder.bind(introSlide[position])
     }
 
     override fun getItemCount(): Int {
@@ -28,6 +31,12 @@ class IntroSlideAdapter(
     inner class IntroSlideViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val textTitle = view.findViewById<TextView>(R.id.textTitle)
         private val textDescription = view.findViewById<TextView>(R.id.textDescription)
+
+        fun bind(introSlide: IntroSlide) {
+            textTitle.text = introSlide.title
+            textDescription.text = introSlide.description
+        }
+    }
 
 
 }
--- app/src/main/java/com/ispace/bootcampispace/start/SliderSlides.kt
+++ app/src/main/java/com/ispace/bootcampispace/start/SliderSlides.kt
@@ -1,4 +1,25 @@
 package com.ispace.bootcampispace.start
 
+import com.ispace.bootcampispace.start.IntroSlide
+
+
 object SliderSlides {
+    val introSliderAdapter = IntroSliderAdapter(
+        listOf(
+            IntroSlide(
+                "Google StartUp",
+                "Learn android development with Google ."
+            ),
+            IntroSlide(
+                "Build project at The end of your learning",
+                "Be selected to sit for the Examination."
+            ),
+            IntroSlide(
+                "Pass the exam",
+                "s"
+            )
+        )
+    )
+
+
 }
